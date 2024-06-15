806b8be326b8c6c5fc9f22677f2daf721484f5dc
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:21:42 +0530

    Update Week-3.09.py

--- Python-moodle-main/Week-3/Week-3.09.py
+++ Python-moodle-main/Week-3/Week-3.09.py
@@ -1,32 +1,11 @@
-Second last digit
-
-Write a program that returns the second last digit of the given number. Second last digit is being referred 10the digit in the tens place in the given number.
-
-For example, if the given number is 197, the second last digit is 9.
-
-Note1 - The second last digit should be returned as a positive number. i.e. if the given number is -197, the second last digit is 9.
-
-Note2 - If the given number is a single digit number, then the second last digit does not exist. In such cases, the program should return -1. i.e. if the given number is 5, the second last digit should be returned as -1.
-
-For example:
-
-Input	Result
-
-197	9
 
 
-
-a=int(input())
-
-b=str(abs(a))
-
-l=len(b)
-
-if(l>1):
-
-    print(int(b[-2]))
-
+mat=int(input())
+phy=int(input())
+che=int(input())
+if (mat>=65 and phy>=55 and che>=50):
+    print("The candidate is eligible")
+elif (mat+phy+che>=180):
+    print("The candidate is eligible")
 else:
-
-    print(-1)
-
+    print("The candidate is not eligible")
