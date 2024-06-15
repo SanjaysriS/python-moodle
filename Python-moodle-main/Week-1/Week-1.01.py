ee6ee64fd167689e46e8bd067dd6b5cc86ce7598
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:49:32 +0530

    Update Week-1.01.py

--- Python-moodle-main/Week-1/Week-1.01.py
+++ Python-moodle-main/Week-1/Week-1.01.py
@@ -1,26 +1,3 @@
-Write a program to convert strings to an integer and float and display its type.
-
-Sample Input:
-
-10
-
-10.9
-
-Sample Output:
-
-10,<class 'int'>
-
-10.9,<class 'float'>
-
-
-
-For example:
-
-Input	Result
-10
-10.9
-10,<class 'int'>
-10.9,<class 'float'>
 
 a=input()
 b=input()
