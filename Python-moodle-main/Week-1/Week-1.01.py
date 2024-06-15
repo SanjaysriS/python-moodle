0635916c0060144fb863de63d390d11beb865c1b
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 17:59:12 +0530

    Update Week-1.01.py

--- Python-moodle-main/Week-1/Week-1.01.py
+++ Python-moodle-main/Week-1/Week-1.01.py
@@ -1,21 +1,35 @@
-
-                      Converting Input Strings
 Write a program to convert strings to an integer and float and display its type.
+
+Sample Input:
+
+10
+
+10.9
+
 Sample Output:
+
+10,<class 'int'>
+
+10.9,<class 'float'>
+
+
+
+For example:
+
+Input	Result
+10
+10.9
 10,<class 'int'>
 10.9,<class 'float'>
 
-For example:
-Input	Result
-10
-10.9	10,<class 'int'>
-10.9,<class 'float'>
-
-
-
 a=input()
 b=input()
-c=int(a)
-d=float(b)
-print(c,type(c),sep=",")
-print("{:0.1f}".format(d),type(d),sep=",")
+a=int(a)
+b=float(b)
+b=round(b,1)
+print(a,end=',')
+print(type(a))
+print(b,end=',')
+print(type(b))
+
+
