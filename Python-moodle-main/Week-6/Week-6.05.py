8172e0bde382f54b8e4bdd7b2b1f53d7c1688216
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:30:26 +0530

    Update Week-6.05.py

--- Python-moodle-main/Week-6/Week-6.05.py
+++ Python-moodle-main/Week-6/Week-6.05.py
@@ -1,40 +1,13 @@
-Remove Palindrome Words
-
-String should contain only the words are not palindrome.
-
-
-
-Sample Input 1
-
-Malayalam is my mother tongue
-
-
-
-Sample Output 1
-
-is my mother tongue
-
-
-
-For example:
-
-
-
- 
-
-
-
-a=[]
-
-a=input()
-
-b=a. split()
-
-for i in b:
-
-    k=i.lower()
-
-    if k!=k[::-1]:
-
-         print(k,end=' ')
-
+n=int(input())
+arr=[]
+for _ in range(n):
+    arr.append(int(input()))
+tot=sum(arr)
+left=0
+right=tot
+for i in range(n):
+    right-=arr[i]
+    if left==right:
+        print(i)
+        break
+    left+=arr[i]
