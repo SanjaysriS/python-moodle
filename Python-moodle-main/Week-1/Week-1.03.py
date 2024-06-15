76b4b5d9d45c5281e22970fbbb09ae246edef240
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:01:10 +0530

    Update Week-1.03.py

--- Python-moodle-main/Week-1/Week-1.03.py
+++ Python-moodle-main/Week-1/Week-1.03.py
@@ -1,10 +1,4 @@
-Square Root
-
 Write a simple python program to find the square root of a given floating point number. The output should be displayed with 3 decimal places.
-
-
-
-
 
 Sample Input:
 
@@ -14,19 +8,19 @@ Sample Output:
 
 2.828
 
+
+
+
+
 For example:
 
 Input	Result
-
-14.00	3.742
+14.00
+3.742
 
 
 
 import math
-
 a=float(input())
-
-s=math.sqrt(a)
-
-print("{:.3f}".format(s))
-
+a=math.sqrt(a)
+print("{:.3f}".format(a))
