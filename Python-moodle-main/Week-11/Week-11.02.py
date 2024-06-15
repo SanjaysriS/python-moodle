1b98951a364d4aaaba07f7177ae5896f432b8206
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:46:01 +0530

    Update Week-11.02.py

--- Python-moodle-main/Week-11/Week-11.02.py
+++ Python-moodle-main/Week-11/Week-11.02.py
@@ -1,48 +1,15 @@
-Problem Description:
+def safe_division():
+    try:
+        num1 = float(input())
+        num2 = float(input())
 
-Write a Python program that asks the user for their age and prints a message based on the age. Ensure that the program handles cases where the input is not a valid integer.
+        result = num1 / num2
 
+        print(result)
 
+    except ZeroDivisionError:
+        print("Error: Cannot divide or modulo by zero.")
+    except ValueError:
+        print("Error: Non-numeric input provided.")
 
-Input Format:
-
-A single line input representing the user's age.
-
-
-
-Output Format:
-
-Print a message based on the age or an error if the input is invalid.
-
-For example:
-
-Input	Result
-
-25	You are 25 years old.
-
-rec	Error: Please enter a valid age.
-
--5	Error: Please enter a valid age.
-
-
-
-try:
-
-    a=input()
-
-    if(len(a)==0):
-
-        print("Error: Please enter a valid age.")
-
-    elif a.isnumeric():
-
-        print("You are",a,"years old.")
-
-    else:
-
-        print("Error: Please enter a valid age.")
-
-except:
-
-    print("Error: Please enter a valid age.")
-
+safe_division()
