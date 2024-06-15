c40aa9bb9216f106cb0044f5e428a5c8bb47b24a
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:30:51 +0530

    Update Week-6.06.py

--- Python-moodle-main/Week-6/Week-6.06.py
+++ Python-moodle-main/Week-6/Week-6.06.py
@@ -1,60 +1,23 @@
-Return Second World in Uppercase
+def factors_of_number(n):
+    factors = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            factors.append(i)
+            if n // i != i:
+                factors.append(n // i)
+    factors.sort()
+    return factors
 
+def find_pth_factor(n, p):
+    factors = factors_of_number(n)
+    if p <= len(factors):
+        return factors[p - 1]
+    else:
+        return 0
 
+# Input
+n = int(input())
+p = int(input())
 
-Write a program that takes as input a string (sentence), and returns its second word in uppercase.
-
-
-
-For example:
-
-
-
-If input is “Wipro Technologies Bangalore” the function should return “TECHNOLOGIES”
-
-If input is “Hello World” the function should return “WORLD”
-
-If input is “Hello” the program should return “LESS”
-
-
-
-NOTE 1: If input is a sentence with less than 2 words, the program should return the word “LESS”.
-
-NOTE 2: The result should have no leading or trailing spaces.
-
-
-
-For example:
-
-
-
-Input	Result
-
-Wipro Technologies Bangalore
-
-TECHNOLOGIES
-
-Hello World
-
-WORLD
-
-Hello
-
-LESS
-
-
-
-f=input()
-
-s=f.split()
-
-if len(s)>1:
-
-    c=s[1]
-
-    print(c.upper())
-
-else:
-
-    print("LESS")
-
+# Output
+print(find_pth_factor(n, p))
