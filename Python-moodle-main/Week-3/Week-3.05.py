630334eebb0dd5591e3c53f142ed29c97be3a232
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:19:30 +0530

    Update Week-3.05.py

--- Python-moodle-main/Week-3/Week-3.05.py
+++ Python-moodle-main/Week-3/Week-3.05.py
@@ -1,58 +1,8 @@
-Vowel or Consonant
-
-In this exercise you will create a program that reads a letter of the alphabet from the user. If the user enters a, e, i, o or u then your program should display a message indicating that the entered letter is a vowel. If the user enters 'y' then your program should display a message indicating that sometimes y is a vowel, and sometimes y is a consonant. Otherwise your program should display a message indicating that the letter is a consonant.
-
-Sample Input 1
-
-i
-
-Sample Output 1
-
-It's a vowel.
-
-Sample Input 2
-
-y
-
-Sample Output 2
-
-Sometimes it's a vowel... Sometimes it's a consonant.
-
-Sample Input3
-
-c
-
-Sample Output 3
-
-It's a consonant.
 
 
-
-
-
-For example:
-
-Input	Result
-
-y	Sometimes it's a vowel... Sometimes it's a consonant.
-
-u	It's a vowel.
-
-p	It's a consonant.
-
-
-
-a=input()
-
-if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
-
-    print("It's a vowel.")
-
-elif(a=='y'):
-
-    print("Sometimes it's a vowel... Sometimes it's a consonant.")
-
+number = int(input())
+if abs(number) < 10:
+    print(-1)
 else:
-
-    print("It's a consonant.")
-
+    second_last = abs(number) // 10 % 10
+    print(second_last)
