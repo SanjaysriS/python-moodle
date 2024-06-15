f57f5f09ee64742ec445c7aa3632e408ac9137a5
SanjaysriS <231001181@rajalakshmi.edu.in>
2024-06-15 18:09:58 +0530

    Update Week-2.04.py

--- Python-moodle-main/Week-2/Week-2.04.py
+++ Python-moodle-main/Week-2/Week-2.04.py
@@ -1,38 +1,17 @@
-Hamming Weight
+Pretend that you have just opened a new savings account that earns 4 percent interest per year. The interest that you earn is paid at the end of the year, and is added to the balance of the savings account. Write a program that begins by reading the amount of money deposited into the account from the user. Then your program should compute and display the amount in the savings account after 1, 2, and 3 years. Display each amount so that it is rounded to 2 decimal places. Sample Input: 10000 Sample Output: Balance as of end of Year 1: $10400.00. Balance as of end of Year 2: $10816.00. Balance as of end of Year 3: $11248.64.
+For example:
 
-Write a python program that takes a integer between 0 and 15 as input and displays the number of '1' s in its binary form.(Hint:use python bitwise operator.
-
-Sample Input
-
-3
-
-Sample Output:
-
-2
-
-Explanation:
-
-The binary representation of 3 is 011, hence there are 2 ones in it. so the output is 2.
+Input	Result
+10000
+Balance as of end of Year 1: $10400.00.
+Balance as of end of Year 2: $10816.00.
+Balance as of end of Year 3: $11248.64.
 
 
 
-a=int(input())
 
-n=bin(a)
-
-n=n.replace("0b","")
-
-s=str(n)
-
-c=list(s)
-
-d=0
-
-for i in range(len(c)):
-
-    if(int(c[i])==1):
-
-        d+=1
-
-print(d)
-
+deposit = float(input())
+interest_rate = 0.04
+for year in range(1, 4):
+    balance = deposit * (1 + interest_rate) ** year
+    print(f"Balance as of end of Year {year}: ${balance:.2f}.")
